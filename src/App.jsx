c8f0a3dd8aa51ea175a66e9608563224e8d148d9
Santiago <santiagovillar@MacBook-Pro-de-Santiago.local>
2024-10-08 10:49:57 +0100

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+export function App () {
+    return (
+        <article>
+            <header>
+                <img alt="avatar midudev" src="https://unavatar.io/kikobeats"/>
+            <div>
+                <strong>Nombre</strong>
+                <span>@Nombre</span>
+            </div>
+            </header>
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { App } from './App'
 
-const createButton = ({text}) => {
-  return(
-    <button> {text} </button>
-  )
-}
 
 const root = createRoot(document.getElementById('root'))
 root.render(
-  <React.Fragment>
-    {createButton({ text: 'Button 1'})}
-    {createButton({ text: 'Button 1'})}
-  </React.Fragment>
+ <App/>
   
 )
