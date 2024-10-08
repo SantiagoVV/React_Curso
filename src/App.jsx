71882db33efdfea91134db0b64e525ef5dacb584
Santiago <santiagovillar@MacBook-Pro-de-Santiago.local>
2024-10-08 17:53:06 +0100

    Componente Follow

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 import './App.css'
+import { FollowCard } from './followCard'
 export function App () {
     return (
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img className='tw-followCard-avatar' alt="avatar midudev" src="https://unavatar.io/kikobeats"/>
-            <div className='tw-followCard-info'>
-                <strong>Nombre</strong>
-                <span className='tw-followCard-infoUsername'>@Nombre</span>
-            </div>
-            </header>
-
-            <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
-                </button>
-            </aside>
-        </article>
+        <section className = "App">
+            <FollowCard userName="kikobeats" name= "Kiks"/>
+            <FollowCard userName="kikobeats" name= "Kiks 2"/>
+        </section>
     )
 }
---
+++ src/FollowCard.jsx
@@ -0,0 +1,22 @@
+export function FollowCard ({userName, name, isFollowing}) {
+    const imageSrc = 'https://unavatar.io/${userName}'
+    
+    return (
+    <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-avatar' alt="avatar midudev"
+                 src= {imageSrc}/>  {/* src = {'https://unavatar.io/${userName}'} */}
+            <div className='tw-followCard-info'>
+                <strong>{name}</strong>
+                <span className='tw-followCard-infoUsername'>{userName}</span>
+            </div>
+            </header>
+
+            <aside>
+                <button className='tw-followCard-button'>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
