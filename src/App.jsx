2bc3af7739f4064503a887d077b5726dcdcdf5c1
Santiago <santiagovillar@MacBook-Pro-de-Santiago.local>
2024-10-14 19:24:15 +0100

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
+import { useState } from 'react'
 import './App.css'
 import { FollowCard } from './followCard.jsx'
 
 export function App () {
 const formatUserName = (userName) => `@${userName}`
 const kiks2 = { formatUserName, userName : "kikobeats"}   
+const [name, setName] = useState('kikobeats')
+
+console.log('render with name:' , name)
 
     return (
         <section className = "App">
             <FollowCard 
                 formatUserName={formatUserName}  
-                userName="kikobeats" >  {/* isFollowing = {true} name= "Kiks "/>  (Prop isFollowing) */}
+                userName={name} >  {/* isFollowing = {true} name= "Kiks "/>  (Prop isFollowing) */}
                 <h3>Kiks</h3>
             </FollowCard>
             
             <FollowCard {... kiks2} > 
                 <h3>Kiks 2</h3>
             </FollowCard>
+
+            <button onClick={()  => setName('pedromichel')}>Cambio nombre</button>
         </section>
     )
 }
